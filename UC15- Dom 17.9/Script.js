e8770e31bce9b0031b809119a3deaf05627c056f
nascimentomiro76-prog <nascimentomiro76@gmail.com>
2026-09-17 10:35:50 -0300

    Add files via upload

---
+++ UC15- Dom 17.9/Script.js
@@ -0,0 +1,7 @@
+function mudarTexto(){
+document.getElementById ("titulo"). textContent = "Você clicou no botão!";
+}
+
+function mudarCor() {
+    document.getElementById ("mensagem").style.color = "blue";
+}
